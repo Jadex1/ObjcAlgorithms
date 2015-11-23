105bd0b8418c43b03684f53bc8c107b8108a6143
James Hall <greatwolf3d@gmail.com>
2015-11-23 13:13:26 -0800

    Added Problem Statement and Input Statement.

--- Algorithms/main.m
+++ Algorithms/main.m
@@ -4,13 +4,15 @@
 //
 //  Created by James Hall on 11/12/15.
 //  Copyright © 2015 James Hall. All rights reserved.
-//
-
+//Your teacher has given you the task of drawing a staircase structure. Being an expert programmer,
+//you decided to make a program to draw it for you instead. Given the required height, can you print a staircase as shown in the example?
+// 1). Input "N".
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
+        //
         NSLog(@"Hello, World!");
     }
     return 0;
