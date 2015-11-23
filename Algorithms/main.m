5ca3f7d0eef95985c77e12567aa59e95d6ad7cda
James Hall <greatwolf3d@gmail.com>
2015-11-23 11:03:21 -0800

    Added the problem statement

--- Algorithms/main.m
+++ Algorithms/main.m
@@ -1,10 +1,14 @@
 //
 //  main.m
 //  Algorithms
-//  Plus Minus 
+//  Plus Minus
 //  Created by James Hall on 11/12/15.
 //  Copyright © 2015 James Hall. All rights reserved.
-//
+/*  ----------------------------------------------------------------------------------------------------------------- */
+//  Problem Statement
+//  You're given an array of integers. Can you find what fraction of the elements are positive numbers, negative numbers and zeroes?
+//  Print the value of the fractions in decimal form with 3 decimal places.
+/*  ----------------------------------------------------------------------------------------------------------------- */
 
 #import <Foundation/Foundation.h>
 int main(int argc, const char * argv[]) {
